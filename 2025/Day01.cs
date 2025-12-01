478de2413411bec028a23143e5c57a380c208aaa
Mark Gross <mark.gross2001@gmail.com>
2025-11-30 20:23:51 -0800

    get ready

---
+++ 2025/Day01.cs
@@ -0,0 +1,16 @@
+using AdventOfCodeSupport;
+
+namespace AoC_2025._2025;
+
+public class Day01 : AdventBase
+{
+    protected override object InternalPart1()
+    {
+        return 1;
+    }
+
+    protected override object InternalPart2()
+    {
+        return 2;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCodeSupport;
+
+var solutions = new AdventSolutions();
+var today = solutions.GetMostRecentDay(2025);
+
+today.Part1().Part2();
+// today.Benchmark();
