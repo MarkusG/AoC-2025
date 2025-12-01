ea9261bce9bdc8405e86dd77e86ebcf889935a49
Mark Gross <mark.gross2001@gmail.com>
2025-11-30 21:29:56 -0800

    day 01

--- 2025/Day01.cs
+++ 2025/Day01.cs
@@ -6,11 +6,51 @@ public class Day01 : AdventBase
 {
     protected override object InternalPart1()
     {
-        return 1;
+        var lines = Input.Lines;
+        var pos = 50;
+        var zeroes = 0;
+        foreach (var l in lines)
+        {
+            if (l[0] == 'L')
+                pos -= int.Parse(l[1..]);
+            else
+                pos += int.Parse(l[1..]);
+            pos %= 100;
+            if (pos == 0)
+                zeroes++;
+        }
+
+        return zeroes;
     }
 
     protected override object InternalPart2()
     {
-        return 2;
+        var lines = Input.Lines;
+        var pos = 50;
+        var zeroes = 0;
+        foreach (var l in lines)
+        {
+            var magnitude = int.Parse(l[1..]);
+            var direction = 1;
+            if (l[0] == 'L')
+                direction = -1;
+            
+            // fuck it
+            for (var i = 0; i < magnitude; i++)
+            {
+                pos += direction;
+
+                if (pos == 100)
+                    pos = 0;
+                
+                if (pos == 0)
+                    zeroes++;
+
+                if (pos == -1)
+                    pos = 99;
+            }
+        }
+
+        return zeroes;
     }
 }
